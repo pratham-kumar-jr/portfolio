557a533c0c2d2ef4b5d375da08e58166a181bfc1
pratham_kumar <pratham.kumar@devslane.com>
2025-09-07 22:35:30 +0530

    changes

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  base: "portfolio",
+  base: "/portfolio/",
   plugins: [react(), mode === "development" && componentTagger()].filter(
     Boolean
   ),
